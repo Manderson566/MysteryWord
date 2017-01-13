110275bf7ee852a1915eb8f967ed25780d6ccbdf
Michael Anderson <Micmail566@gmail.com>
2017-01-13 15:58:05 -0500

    need to fix letter guessing.

--- Program.cs
+++ Program.cs
@@ -9,32 +9,67 @@ namespace MysteryWord
 {
     class Program
     {
-        static string RandomList()
+        static string RandomName()
         {
             string[] newArray = File.ReadAllLines(@"../../TextFile1.txt");
             var newList = newArray.ToList<string>();
             var rng = new Random();
-            int randomNumber = rng.Next(newList.Count);
-            string random = newList[randomNumber];
-            Console.WriteLine(newList[randomNumber]);
-            Console.WriteLine(rng.Next(100, 400));
+            int randomName = rng.Next(newList.Count);
+            string random = newList[randomName];
+            //Console.WriteLine(newList[randomName]);
             return random;
 
         }
-    
+
         static void Main(string[] args)
         {
-            //"ABCDEF".ToList<char>();
-            string[] newArray = File.ReadAllLines(@"../../TextFile1.txt");
-             var newList = newArray.ToList<string>();
-            Console.WriteLine(string.Join(" ", newList.GetRange(1, 5)));
-            Console.ReadLine();
+
+
+            string idRname = RandomName();
+            int counter = 0;
+            Console.WriteLine(idRname);
+            foreach (char place in idRname)
             {
-                RandomList();
+                Console.Write("_ ");
+                counter++;
+
             }
+            List<char> checkLet = new List<char>(idRname);
+            //string guessLet = new string(idrname)
+            string letter = Console.ReadLine();
+            for (int i = 0; i < idRname.Count(); i++)
+            {
+
+                if (letter == idRname.ToString())
+                {
+                    Console.WriteLine("correct");
+                }
+            }
+
+
+
         }
 
-       
-        
+
+
+
+
+
     }
+    // string idRname = RandomName();
+    // string replaceLetter = alph.Remove('a');
+    //Console.WriteLine($"{replaceLetter}***");
+    //Console.WriteLine(string.Join(" ",alph));
+
+
+
+
+    //char newChar = "ABCD".ToList<char>();
+
+
 }
+
+
+
+
+
